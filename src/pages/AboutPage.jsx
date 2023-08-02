7c8fcbb0304f92c486ac2d88e331aedea19e9353
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-08-02 00:28:39 -0700

    created context & provider

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Card from '../components/shared/Card'
 
 function AboutPage() {
@@ -6,10 +7,10 @@ function AboutPage() {
     <Card>
       <div className='about'>
         <h1>About This Project</h1>
-        <p>This is a React app to leave a feedback fro a product or service</p>
+        <p>This is a React app to leave a feedback for a product or service</p>
         <p>Version: 1.0.0</p>
         <p>
-          <a href='/'>Back To Home</a>
+          <Link to='/'>Back To Home</Link>
         </p>
       </div>
     </Card>
